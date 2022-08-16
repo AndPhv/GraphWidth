21400043dc637f6ab666b09de9ffd6f99bd39197
AndPhv <puhov2001@gmail.com>
2022-08-16 13:04:00 +0300

    Создание методов по добавлению вершин и ребер между ними

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -12,4 +12,13 @@ public class Graph {
         matrix = new int[amountV][amountV];
         currentV = 0;
     }
+
+    public void addVertex(String name) {
+        vertexList[currentV++] = new Vertex(name);
+    }
+
+    public void addEdge(int start, int end, int value) {
+        matrix[start][end] = 1;
+        matrix[end][start] = value;
+    }
 }

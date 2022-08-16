f77ad27e2d014b296f9088202e93ff00f9624622
AndPhv <puhov2001@gmail.com>
2022-08-16 13:14:40 +0300

    Переопределение методов из интерфейса

--- src/com/company/GraphQueue.java
+++ src/com/company/GraphQueue.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class GraphQueue {
+public class GraphQueue <T> implements Queue<T> {
     private int[] array;
     private int size = 10;
     private int head;
@@ -12,5 +12,28 @@ public class GraphQueue {
         head = 0;
         tail = -1;
         count = 0;
+    }
+
+    @Override
+    public void add(int T) {
+        if (tail == size - 1)
+            tail = -1;
+
+        array[++tail] = T;
+        count++;
+    }
+
+    @Override
+    public int remove() {
+        if (head == size)
+            head = 0;
+
+        count--;
+        return array[head++];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
     }
 }

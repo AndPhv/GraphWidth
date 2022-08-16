ec88ca889b5e681f65f320ee7e335b4716cc557c
AndPhv <puhov2001@gmail.com>
2022-08-16 13:41:55 +0300

    Создание экземпляра Graph и метода для вывода результата с пользовательским вводом

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Graph graph = new Graph();
+
+        print(graph);
+    }
+    public static void print(Graph graph) {
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        int y;
+
+        System.out.print("Введите количество вершин в графе - ");
+        graph.amountV = scanner.nextInt();
+
+        for (int i = 0; i < graph.amountV; i++) {
+            System.out.print("Введите вершину - " );
+            graph.addVertex(scanner.next());
+            System.out.println("Номер вершины равен " + i);
+        }
+
+        for (int i = 0; i < graph.amountV - 1; i++) {
+            System.out.print("Соединить вершину ");
+            x = scanner.nextInt();
+            System.out.print(" с вершиной ");
+            y = scanner.nextInt();
+            graph.addEdge(x, y, 1);
+        }
+        graph.passInWidth(0);
     }
 }

50e5b584b2621905fd96f3ee43dbd1eef72cb6b8
AndPhv <puhov2001@gmail.com>
2022-08-16 13:04:39 +0300

    Создание метода по проверке посещенности вершин

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -21,4 +21,12 @@ public class Graph {
         matrix[start][end] = 1;
         matrix[end][start] = value;
     }
+
+    public int check(int v) {
+        for (int i = 0; i < currentV; i++) {
+            if (matrix[v][i] == 1 && vertexList[i].isVisited == false)
+                return i;
+        }
+        return -1;
+    }
 }
